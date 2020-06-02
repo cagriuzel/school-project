d462bf4167c042ae60b5fefbd28ed643b144ec76
Çağrı ÜZEL <cagriuzel@gmail.com>
2020-06-03 01:40:36 +0300

    core-ui added

---
+++ school-project/src/main/java/com/mmc/kbs/controller/HomeController.java
@@ -0,0 +1,19 @@
+package com.mmc.kbs.controller;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class HomeController {
+
+	@Value("${spring.application.name}")
+	private String applicationName;
+	
+	@GetMapping("/")
+	public String home(Model model) {
+		model.addAttribute("appName", applicationName);
+		return "home";
+	}
+}
