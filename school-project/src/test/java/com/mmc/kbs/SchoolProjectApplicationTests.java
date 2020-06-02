b815756d3e0a3886bc8648c348897049fd733dd4
Çağrı ÜZEL <cagriuzel@gmail.com>
2020-06-02 22:12:37 +0300

    hello world

---
+++ school-project/src/test/java/com/mmc/kbs/SchoolProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mmc.kbs;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SchoolProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
